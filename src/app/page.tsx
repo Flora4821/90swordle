a262623796ef448e06ec02a31c3766f3a7f3ce5d
Floor4821 <floor.vanmoorsel@gmail.com>
2025-03-31 12:19:26 +0200

    Small amount of progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 export default function Home() {
+  const rows = 6;
+  const cols = 5;
+  
+
+
   return (
   <div>
-    <div className="flex flex-row justify-center">
-      <input type="text" placeholder="Test"></input>
-      <input type="text" placeholder="Test"></input>
-      <input type="text" placeholder="Test"></input>
-      <input type="text" placeholder="Test"></input>
+    <div className="grid content-center">
+      {Array.from({ length: rows}).map((_, index) => (
+        <div key={index}>
+          {Array.from({length: cols}).map((_, colIndex)=> (<input key={colIndex}/>))}
+        </div>))
+      }
     </div>
   </div>
+
   );
 }
